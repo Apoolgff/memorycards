b97451c08248941d95f164d41d69c4bbb3905d21
Paolo Ferro <pgff_9@hotmail.com>
2023-12-31 11:26:21 -0300

    Footer Desactivado

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,18 @@ function App() {
   const boardRef = useRef();
 
   const restartGame = () => {
-    
+
     boardRef.current.initializeGame();
   };
 
   return (
-    <div className="app">
-      <h1>Memory Cards Game</h1>
-      <button className="btn" onClick={restartGame}>Restart Game</button>
-      <Board ref={boardRef} />
-    </div>
+    <>
+      <div className="app">
+        <h1>Memory Cards Game</h1>
+        <button className="btn" onClick={restartGame}>Restart Game</button>
+        <Board ref={boardRef} />
+      </div>
+    </>
   );
 }
 
